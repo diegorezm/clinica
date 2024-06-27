098d9cc9327bc17e5d45a5450c7e79d2a52e5784
diegorezm <dmourarezende@gmail.com>
2024-06-27 13:15:21 -0300

    fontend init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dir, "/src"),
+      "@server": path.resolve(import.meta.dir, "../server"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
