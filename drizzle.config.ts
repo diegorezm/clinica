c37f7a5cb39950d89e32e10bb3f04b66d2b72d1c
diegorezm <dmourarezende@gmail.com>
2024-06-27 21:33:02 -0300

    configured testing db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 
+const DB_URL = process.env.DB_URL!;
+const TEST_DB_URL = process.env.TEST_DB_URL!;
+const NODE_ENV = process.env.NODE_ENV || "dev";
+const CONN_STR = NODE_ENV === "dev" ? TEST_DB_URL : DB_URL;
+
 export default defineConfig({
   schema: "./server/db/schemas/*",
   out: "./drizzle",
   dialect: "postgresql", // 'postgresql' | 'mysql' | 'sqlite'
   dbCredentials: {
     host: process.env.DB_HOST,
-    url: process.env.DB_URL,
+    url: CONN_STR,
   },
 });
--- server/db/index.ts
+++ server/db/index.ts
@@ -2,12 +2,22 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres = require("postgres");
 
 const DB_URL = process.env.DB_URL;
+const TEST_DB_URL = process.env.TEST_DB_URL;
+const NODE_ENV = process.env.NODE_ENV || "dev";
 
-if (!DB_URL) {
+if (!DB_URL && !TEST_DB_URL) {
   throw new Error("No database set.");
 }
 
-const client = postgres(DB_URL);
+let conn_string = NODE_ENV === "production" ? DB_URL : TEST_DB_URL;
+
+if (!conn_string) {
+  throw new Error(
+    "No valid connection string set for the current environment.",
+  );
+}
+
+const client = postgres(conn_string);
 const db = drizzle(client);
 
 export default db;
