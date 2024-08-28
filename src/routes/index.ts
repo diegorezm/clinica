d344de92955dfec9ff190b833996d0df9890bf83
diegorezm <dmourarezende@gmail.com>
2024-08-28 11:19:18 -0300

    deleted index

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const paginatedRequestSchema = z.object({
-  q: z.string().optional(),
-  page: z.coerce.number().default(1),
-  size: z.coerce.number().default(10),
-});
